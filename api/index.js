d29ba9f7e136bc7bc2473d004b46cfdce2fa95fe
Abdallahi Nah <abdallahi.nah.un.fst@gmail.com>
2025-08-04 16:54:04 +0000

    create a middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -26,3 +26,14 @@ app.listen(3000, () => {
 // api routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+// middlewares
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
