73a101141dbac50d33cbe8d91f97de32e8214968
Abdallahi Nah <abdallahi.nah.un.fst@gmail.com>
2025-08-04 08:44:43 +0000

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("database connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.listen(3000, () => {
-    console.log("server is running on port 3000");
+  console.log("server is running on port 3000");
 });
